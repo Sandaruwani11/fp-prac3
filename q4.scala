67eba16ddf1229be0639c32e987161cc250068e1
Sandaruwani11 <155408597+Sandaruwani11@users.noreply.github.com>
2024-07-02 08:47:51 +0530

    q4.scala

--- q4.scala
+++ q4.scala
@@ -8,7 +8,6 @@ object SumOfEvenNumbers{
     val numbers = List(2, 11, 24, 5, 16, 7, 14, 8, 19, 45)
     val sumOfEvens = sumOfEvenNumbers(numbers)
     
-    println(s"List: ${numbers.mkString(", ")}")
     println(s"Sum of even numbers: $sumOfEvens")
   }
 }
